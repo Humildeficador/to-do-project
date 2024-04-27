9a8a8e9903f48bd005431ef7f75fbb49e6af17c9
João Leandro <joao.oliveira1270@etec.sp.gov.br>
2024-04-27 01:37:35 -0300

    change icon path

--- src/components/Task/TaskToDo/TaskToDo.tsx
+++ src/components/Task/TaskToDo/TaskToDo.tsx
@@ -1,7 +1,5 @@
 import { ChangeEvent, ComponentProps, useState } from "react";
 
-const icon = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0OCIgaGVpZ2h0PSI0OCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iNCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1jaGVjayI+PHBhdGggZD0iTTIwIDYgOSAxN2wtNS01Ii8+PC9zdmc+'
-
 export function TaskToDo({ children }: ComponentProps<'label'>) {
     const [isChecked, setIsChecked] = useState(false);
 
@@ -12,12 +10,13 @@ export function TaskToDo({ children }: ComponentProps<'label'>) {
 
     return (
         <div>
-            <label className='flex items-center gap-2' >
+            <label className='flex items-center gap-5' >
                 <input
                     type="checkbox"
                     className={`
+                        peer
                         custom-checkbox
-                        size-4
+                        size-6
                         !border-slate-600/50
                         rounded-[0.25rem]
                         focus-within:ring-2
@@ -27,10 +26,15 @@ export function TaskToDo({ children }: ComponentProps<'label'>) {
                         focus-within:ring-indigo-600
                         checked:bg-indigo-600
                         ease-in-out
-                        checked:after:bg-[url('${icon}')]`}
+                        checked:after:bg-[url('/check.svg')]`}
                     onChange={handleCheck} />
-                <span className={`text-sm ${isChecked && 'line-through'}`}>{children}</span>
+                <span className={`
+                    text-xl 
+                    ${isChecked && 'line-through'}
+                    peer-checked:text-slate-900/50 dark:peer-checked:text-slate-400/50
+                `}>{children}</span>
             </label>
         </div>
+
     )
 }
